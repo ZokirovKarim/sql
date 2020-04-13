6f18a2e6d340a85b7ec35c948c8cd424d2c3816a
Karim <karim.zokirov@mail.ru>
2020-04-13 18:19:23 +0500

    SQL DZ

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace sql
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+        }
+    }
+    class school
+    {
+        public int Id;
+        public string FristName;
+        public string LastName;
+        public string MiddleName;
+        public int age;
+       
+    }
+}
